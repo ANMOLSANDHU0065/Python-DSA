ceaf01400ab73711a1a1b40b22549d6d48b14da1
Anmol Sandhu <singhanmol0359@gmail.com>
2026-09-19 10:04:17 +0530

    Add files via upload

---
+++ Day-2.py
@@ -0,0 +1,17 @@
+# 2. PALINDROME NUMBERS
+
+class Solution:
+    def isPalindrome(self, x):
+        
+        if x < 0:
+            return False
+        
+        original = x
+        reverse = 0
+        
+        while x > 0:
+            digit = x % 10
+            reverse = reverse * 10 + digit
+            x = x // 10
+        
+        return original == reverse
