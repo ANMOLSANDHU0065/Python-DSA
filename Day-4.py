325eb9af89f51760545f7d7eb5accb91cb361c59
Anmol Sandhu <singhanmol0359@gmail.com>
2026-09-20 09:49:54 +0530

    Add files via upload

---
+++ Day-4.py
@@ -0,0 +1,13 @@
+
+# Reverse-degree-of-a-string
+
+
+class Solution:
+    def reverseDegree(self, s):
+        total = 0
+
+        for i in range(len(s)):
+            value = 26 - (ord(s[i]) - ord('a'))
+            total += value * (i + 1)
+
+        return total
