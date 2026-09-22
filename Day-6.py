eb6388f2e1d05a5a7622ccc20ebc4862f7a73ed0
Anmol Sandhu <singhanmol0359@gmail.com>
2026-09-22 07:27:12 +0530

    Add files via upload

---
+++ Day-6.py
@@ -0,0 +1,15 @@
+# Longest Common Prefix
+
+
+class Solution:
+    def longestCommonPrefix(self, strs):
+        prefix = strs[0]
+
+        for word in strs[1:]:
+            while not word.startswith(prefix):
+                prefix = prefix[:-1]
+
+                if prefix == "":
+                    return ""
+
+        return prefix
